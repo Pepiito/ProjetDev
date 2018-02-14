c97db1fee5ddc183657cdb6f6c3fde648a661695
Pepito <benoit.messiaen@ensg.eu>
2018-02-14 17:02:02 +0100

    ./

---
+++ php/head.php
@@ -0,0 +1,16 @@
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>Projet Dev</title>
+    <meta name="author" content="Benoit Messiaen, Hugo Lecomte">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="./images/terre.png" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto:500" rel="stylesheet">
+    <link rel="stylesheet" href="./css/style.css">
+    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.2/dist/leaflet.css" />
+    <script src="https://unpkg.com/leaflet@1.0.2/dist/leaflet.js"></script>
+    <script src='https://code.jquery.com/jquery-3.1.0.min.js'></script>
+    <script src='./js/main.js'></script>
+  </head>
