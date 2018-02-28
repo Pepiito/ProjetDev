771a000c7b4130634d4cc528a5c95019bfbb9a1a
Pepito <benoit.messiaen@ensg.eu>
2018-02-28 16:10:54 +0100

    documentation

--- php/modele/transfo_coord.php
+++ php/modele/transfo_coord.php
@@ -3,5 +3,11 @@
 
 <?php // Récupère les variables AJAX dans la variable $_POST au format convenu ?>
 
-<?php // Renvoie une chaine de caractère au format var1=foo&var2=bar.
-      // L'ajax récupèrera le contenu intégral de ce qui est renvoyé ?>
+<?php  /* Renvoie une chaine de caractère au format var1=foo&var2=bar.
+       L'ajax récupèrera le contenu intégral de ce qui est renvoyé,
+       au foramt "Error XXX: description" si erreur ou "var1=foo&var2=bar" si ok.
+
+       Ce fichier sert de boite de réception / d'envoi de la couche modèle : ne pas le renommer.
+       Le contenu pourra être contenu dans des fichiers externes reliés par un include().
+       */
+       ?>
