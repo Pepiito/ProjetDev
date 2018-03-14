9a993ac9d40a34bce5f9637940c357539b5f9723
Pepito <benoit.messiaen@ensg.eu>
2018-03-14 16:49:10 +0100

    debut transfo fichier

--- js/controleur.js
+++ js/controleur.js
@@ -10,19 +10,46 @@ Les fontions sont définies dans le main.js
 Variables à déclarer
 */
 
-var boutons = Array.from(document.getElementsByTagName('button')); // Liste des boutons au format Array
+window.addEventListener('load', (event) => {
 
+  // variables à déclarer
+
+  window.projection_params = document.getElementsByClassName('projection');
+  window.cartesien_params = document.getElementsByClassName('cartesien');
+  window.geog_params = document.getElementsByClassName('geographique');
+
+  // fonction à éxécuter au lancement
+
+  disable(cartesien_params);
+  disable(geog_params);
+  enable(projection_params);
+}, false);
 
 /*
 Ecouteurs
 */
 
-boutons.forEach( function (bouton) {
-  bouton.addEventListener('click', (event) => {
-    Test();
-  }, false);
-});
-
 document.addEventListener('keydown', (event) => {
   console.log(event.key);
+}, false);
+
+document.getElementById('type_coord_transfo_fichier').addEventListener('change', (event) => {
+  console.log(event.target.value);
+  switch(event.target.value) {
+    case "Projetées":
+      disable(cartesien_params);
+      disable(geog_params);
+      enable(projection_params);
+      break;
+    case "Geog":
+      disable(cartesien_params);
+      enable(geog_params);
+      disable(projection_params);
+      break;
+    case "Cart":
+      enable(cartesien_params);
+      disable(geog_params);
+      disable(projection_params);
+      break;
+  };
 }, false);
