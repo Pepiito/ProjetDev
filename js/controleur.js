2b8a23ae8d08c0d4d5964377c5b4bbe2b3fe5da1
Brounst176 <bruno.dellacasa@heig-vd.ch>
2018-03-20 10:35:23 +0100

    Transformation de coordonnées
    Finalisation de l'interface de transformation de coordonnées

--- js/controleur.js
+++ js/controleur.js
@@ -17,12 +17,33 @@ window.addEventListener('load', (event) => {
   window.projection_params = document.getElementsByClassName('projection');
   window.cartesien_params = document.getElementsByClassName('cartesien');
   window.geog_params = document.getElementsByClassName('geographique');
+  
+  window.alti_suisse = document.getElementsByClassName('Alt_suisse');
+  window.alti_francais = document.getElementsByClassName('Alt_francais');
+  window.alti_suisse2 = document.getElementsByClassName('Alt_suisse2');
+  window.alti_francais2 = document.getElementsByClassName('Alt_francais2');
+  
 
   // fonction à éxécuter au lancement
-
+	
   disable(cartesien_params);
   disable(geog_params);
   enable(projection_params);
+  
+
+  disable(alti_suisse);
+  disable(alti_francais);
+  document.getElementById("altimetrieChoice_alti").disabled = true;
+  document.getElementById("type_altimetre_projetee_coord").style.color = "gray";
+  document.getElementById("type_altimetre_projetee_coord").style.borderColor = "gray";
+  document.getElementById("altimetrieChoice_hauteur").checked = true;
+  document.getElementById('sys_alti_depart_coord').style.display="none";
+  
+  document.getElementById("altimetrieChoice_alti2").disabled = true;
+  document.getElementById("type_altimetre_projetee_coord2").style.color = "gray";
+  document.getElementById("type_altimetre_projetee_coord2").style.borderColor = "gray";
+  document.getElementById("altimetrieChoice_hauteur2").checked = true;
+  document.getElementById('sys_alti_arrivee_coord').style.display="none";
 }, false);
 
 /*
@@ -54,6 +75,10 @@ document.getElementById('type_coord_transfo_fichier').addEventListener('change',
   };
 }, false);
 
+
+
+// Fonctions pour l'affichage dans la popup de transformation de coordonnées pour le système de départ
+// ---------------------------------------------------------------------------------------------------
 document.getElementById('type_coord').addEventListener('change', (event)=> {
 	console.log(event.target.value);
 	switch(event.target.value){
@@ -73,4 +98,158 @@ document.getElementById('type_coord').addEventListener('change', (event)=> {
 			Cart.style.display="block";
 			break;
 	};
+}, false);
+
+document.getElementById('head1').addEventListener('click', (event) => {
+	document.getElementById('trans_coord').style.display="block";
+    document.getElementById('trans_fichier').style.display="none";
+    document.getElementById('head1').style.borderWidth="0px 1px 0px 0px";
+    document.getElementById('head2').style.borderWidth="0px 0px 2px 1px";
+}, false);
+
+document.getElementById('head2').addEventListener('click', (event) => {
+	document.getElementById('trans_coord').style.display="none";
+    document.getElementById('trans_fichier').style.display="block";
+    document.getElementById('head1').style.borderWidth="0px 1px 2px 0px";
+    document.getElementById('head2').style.borderWidth="0px 0px 0px 1px";
+}, false);
+
+document.getElementById('altimetrieChoice_alti').addEventListener('click', (event) => {
+	document.getElementById('sys_alti_depart_coord').style.display="block";
+	document.getElementById("label_input_alti_projetee_coord").innerHTML = "Altitude [m]";
+}, false);
+document.getElementById('altimetrieChoice_hauteur').addEventListener('click', (event) => {
+	document.getElementById('sys_alti_depart_coord').style.display="none";
+	document.getElementById("label_input_alti_projetee_coord").innerHTML = "Hauteur [m]";
+}, false);
+
+document.getElementById('systeme_plani_coord').addEventListener('change', (event) => {
+  console.log(event.target.value);
+  if(event.target.value=="ETRS89"){
+	document.getElementById("altimetrieChoice_alti").disabled = true;
+	document.getElementById("type_altimetre_projetee_coord").style.color = "gray";
+	document.getElementById("type_altimetre_projetee_coord").style.borderColor = "gray";
+	document.getElementById("altimetrieChoice_hauteur").checked = true;
+	document.getElementById('sys_alti_depart_coord').style.display="none";
+	document.getElementById("label_input_alti_projetee_coord").innerHTML = "Hauteur [m]";
+  }else{
+	document.getElementById("altimetrieChoice_alti").disabled = false;
+	document.getElementById("type_altimetre_projetee_coord").style.color = "black";
+	document.getElementById("type_altimetre_projetee_coord").style.borderColor = "black";
+  };
+  switch(event.target.value) {
+    case "ETRS89":
+      disable(alti_suisse);
+      disable(alti_francais);
+	  document.getElementById("altimetrie_RAN95_coord").selected = false; 
+	  document.getElementById("altimetrie_IGN69_coord").selected = false;
+	  
+      break;
+	case "CH1903+":
+      enable(alti_suisse);
+      disable(alti_francais);
+	  document.getElementById("altimetrie_RAN95_coord").selected = true; 
+	  document.getElementById("altimetrie_IGN69_coord").selected = false; 
+      break;
+	case "CH1903":
+      enable(alti_suisse);
+      disable(alti_francais);
+	  document.getElementById("altimetrie_RAN95_coord").selected = true; 
+	  document.getElementById("altimetrie_IGN69_coord").selected = false; 
+      break;
+	case "RGF93":
+      disable(alti_suisse);
+      enable(alti_francais);
+	  document.getElementById("altimetrie_RAN95_coord").selected = false; 
+	  document.getElementById("altimetrie_IGN69_coord").selected = true; 
+      break;
+    case "NTF":
+      disable(alti_suisse);
+      enable(alti_francais);
+	  document.getElementById("altimetrie_RAN95_coord").selected = false; 
+	  document.getElementById("altimetrie_IGN69_coord").selected = true; 
+      break;
+  };
+}, false);
+
+
+// Fonctions pour l'affichage dans la popup de transformation de coordonnées pour le système d'arrivée
+// ---------------------------------------------------------------------------------------------------
+document.getElementById('type_coord2').addEventListener('change', (event)=> {
+	console.log(event.target.value);
+	switch(event.target.value){
+		case "Projetées":
+			Projetées2.style.display="block";
+			Geog2.style.display="none";
+			Cart2.style.display="none";
+			break;
+		case "Geog":
+			Projetées2.style.display="none";
+			Geog2.style.display="block";
+			Cart2.style.display="none";
+			break;
+		case "Cart":
+			Projetées2.style.display="none";
+			Geog2.style.display="none";
+			Cart2.style.display="block";
+			break;
+	};
+}, false);
+
+document.getElementById('altimetrieChoice_alti2').addEventListener('click', (event) => {
+	document.getElementById('sys_alti_arrivee_coord').style.display="block";
+	document.getElementById("label_input_alti_projetee_coord2").innerHTML = "Altitude [m]";
+}, false);
+document.getElementById('altimetrieChoice_hauteur2').addEventListener('click', (event) => {
+	document.getElementById('sys_alti_arrivee_coord').style.display="none";
+	document.getElementById("label_input_alti_projetee_coord2").innerHTML = "Hauteur [m]";
+}, false);
+
+document.getElementById('systeme_plani_coord2').addEventListener('change', (event) => {
+  console.log(event.target.value);
+  if(event.target.value=="ETRS89"){
+	document.getElementById("altimetrieChoice_alti2").disabled = true;
+	document.getElementById("type_altimetre_projetee_coord2").style.color = "gray";
+	document.getElementById("type_altimetre_projetee_coord2").style.borderColor = "gray";
+	document.getElementById("altimetrieChoice_hauteur2").checked = true;
+	document.getElementById('sys_alti_arrivee_coord').style.display="none";
+	document.getElementById("label_input_alti_projetee_coord2").innerHTML = "Hauteur [m]";
+  }else{
+	document.getElementById("altimetrieChoice_alti2").disabled = false;
+	document.getElementById("type_altimetre_projetee_coord2").style.color = "black";
+	document.getElementById("type_altimetre_projetee_coord2").style.borderColor = "black";
+  };
+  switch(event.target.value) {
+    case "ETRS89":
+      disable(alti_suisse2);
+      disable(alti_francais2);
+	  document.getElementById("altimetrie_RAN95_coord2").selected = false; 
+	  document.getElementById("altimetrie_IGN69_coord2").selected = false;
+	  
+      break;
+	case "CH1903+":
+      enable(alti_suisse2);
+      disable(alti_francais2);
+	  document.getElementById("altimetrie_RAN95_coord2").selected = true; 
+	  document.getElementById("altimetrie_IGN69_coord2").selected = false; 
+      break;
+	case "CH1903":
+      enable(alti_suisse2);
+      disable(alti_francais2);
+	  document.getElementById("altimetrie_RAN95_coord2").selected = true; 
+	  document.getElementById("altimetrie_IGN69_coord2").selected = false; 
+      break;
+	case "RGF93":
+      disable(alti_suisse2);
+      enable(alti_francais2);
+	  document.getElementById("altimetrie_RAN95_coord2").selected = false; 
+	  document.getElementById("altimetrie_IGN69_coord2").selected = true; 
+      break;
+    case "NTF":
+      disable(alti_suisse2);
+      enable(alti_francais2);
+	  document.getElementById("altimetrie_RAN95_coord2").selected = false; 
+	  document.getElementById("altimetrie_IGN69_coord2").selected = true; 
+      break;
+  };
 }, false);
--- php/vue/change_coord.php
+++ php/vue/change_coord.php
@@ -21,7 +21,7 @@
 							</div>
 							<div>
 								<label for="systeme">Systèmes</label>
-								<select name="systeme" id="systeme" style="width:150px";>
+								<select name="systeme" id="systeme_plani_coord" style="width:150px";>
 									<optgroup label="Europe">
 										<option value="ETRS89">ETRS89/CHTRF95</option>
 									</optgroup>
@@ -38,44 +38,43 @@
 						</div>
 						<div id="Projetées">
 						
-							<div style="display:flex;">
-							
-								<input type="radio" id="altimetrieChoice1"
-								name="altimetrie_depart" value="altitude" checked>
-								<label for="altimetrieChoice1" style="margin:auto 10px auto 0;">Altitude</label>
-							
-								<input type="radio" id="altimetrieChoice2"
-								name="altimetrie_depart" value="hauteur">
-								<label for="altimetrieChoice2" style="margin:auto 30px auto 0;">Hauteur ellipsoïdale</label>
-							
-								<div>
+							<div style="display:flex;margin:15px 0 15px 0;height:40px;">
+								<div id="type_altimetre_projetee_coord">
+								<input type="radio" id="altimetrieChoice_alti"
+								name="altimetrie_depart" value="altitude">
+								<label for="altimetrieChoice_alti" style="margin:auto 10px auto 0;">Altitude</label>
+								<input type="radio" id="altimetrieChoice_hauteur"
+								name="altimetrie_depart" value="hauteur" checked>
+								<label for="altimetrieChoice_hauteur" style="margin:auto 10px auto 0;" >Hauteur ellipsoïdale</label>
+								</div>
+								<div id="sys_alti_depart_coord">
 									<label for="sys_alt">Système altimétrique</label>
 									<select name="sys_alt" id="sys_alt" style="width:150px";>
 										<optgroup label="Suisse" class="Alt_suisse">
-											<option value="RAN95">RAN95</option>
-											<option value="NF02">NF02</option>
-										</optgroup class="Alt_francais">
-										<optgroup label="France">
-											<option value="">IGN69</option>
+											<option value="RAN95" id="altimetrie_RAN95_coord">RAN95</option>
+											<option value="NF02" id="altimetrie_NF02_coord">NF02</option>
+										</optgroup>
+										<optgroup label="France" class="Alt_francais">
+											<option value="IGN69" id="altimetrie_IGN69_coord">IGN69</option>
 										</optgroup>
 									</select>
 								</div>
 							</div>
 							<div style="display:flex;">
 								<div>
-									<label for="Est">Est</label>
+									<label for="Est">Est [m]</label>
 									<input type="text" name="Est">
 								</div>
 								<div>
-									<label for="Nord">Nord</label>
-									<input type="text" name="Est">
+									<label for="Nord">Nord [m]</label>
+									<input type="text" name="Nord">
 								</div>
 								<div>
-									<label for="Altitude">Altitude</label>
-									<input type="text" name="Est">
+									<label for="Altitude" id="label_input_alti_projetee_coord">Hauteur [m]</label>
+									<input type="text" name="H">
 								</div>
 								<div>
-									<label for="verticale" style="display:flex">Déviation de la véritcale<p style="font-family: 'Greek', Sherif;margin:0;">c-x</p></label>
+									<label for="verticale" style="display:flex">Déviation de la véritcale &eta; - &xi;</label>
 									<input type="text" name="Vert1" id="vert">
 									<input type="text" name="Vert2" id="vert">
 								</div>
@@ -86,7 +85,7 @@
 							</div>
 						</div>
 						<div id="Geog">
-							<div style="display:flex;">
+							<div style="display:flex; margin:15px 0 0 0;">
 								<div>
 									<label for="Long">Long</label>
 									<input type="text" name="Long">
@@ -99,20 +98,28 @@
 									<label for="hauteur">hauteur</label>
 									<input type="text" name="hauteur">
 								</div>
+								<div>
+									<label for="geog_unite_depart">Unités</label>
+									<select name="geog_unite_depart" id="geog_unite_coord" style="width:150px";>
+										<option value="grade" id="geog_unite_coord_grade">Grades</option>
+										<option value="degre" id="geog_unite_coord_degre">Degrés centésimaux</option>
+										<option value="rad" id="geog_unite_coord_rad">Radians</option>
+									</select>
+								</div>
 							</div>
 						</div>
 						<div id="Cart">
-							<div style="display:flex;">
+							<div style="display:flex;margin:15px 0 0 0;">
 								<div>
-									<label for="X">X</label>
+									<label for="X">X [m]</label>
 									<input type="text" name="X">
 								</div>
 								<div>
-									<label for="Y">Y</label>
+									<label for="Y">Y [m]</label>
 									<input type="text" name="Y">
 								</div>
 								<div>
-									<label for="Z">Z</label>
+									<label for="Z">Z [m]</label>
 									<input type="text" name="Z">
 								</div>
 							</div>
@@ -123,7 +130,7 @@
 						<div style="display:flex;">
 							<div>
 								<label for="type_coord2">Type de coordonnées</label>
-								<select name="type_coord2" onchange="change_typecoord2(this.value)" id="type_coord2" style="margin-right:20px;">
+								<select name="type_coord2" id="type_coord2" style="margin-right:20px;">
 									<option value="Projetées" selected>Projetées</option>
 									<option value="Geog">Géographiques</option>
 									<option value="Cart">Cartésiennes</option>
@@ -131,8 +138,10 @@
 							</div>
 							<div>
 								<label for="systeme2">Système</label>
-								<select name="systeme2" id="systeme2" style="width:150px";>
-									<option value="ETRS89">ETRS89</option>
+								<select name="systeme2" id="systeme_plani_coord2" style="width:150px";>
+									<optgroup label="Europe">
+										<option value="ETRS89">ETRS89/CHTRF95</option>
+									</optgroup>
 									<optgroup label="Suisse">
 										<option value="CH1903+">CH1903+</option>
 										<option value="CH1903">CH1903</option>
@@ -144,7 +153,102 @@
 								</select>
 							</div>
 						</div>
+						<div id="Projetées2">
+						
+							<div style="display:flex;margin:15px 0 15px 0;height:40px;">
+								<div id="type_altimetre_projetee_coord2">
+								<input type="radio" id="altimetrieChoice_alti2"
+								name="altimetrie_depart2" value="altitude">
+								<label for="altimetrieChoice_alti2" style="margin:auto 10px auto 0;">Altitude</label>
+								<input type="radio" id="altimetrieChoice_hauteur2"
+								name="altimetrie_depart2" value="hauteur" checked>
+								<label for="altimetrieChoice_hauteur2" style="margin:auto 10px auto 0;" >Hauteur ellipsoïdale</label>
+								</div>
+								<div id="sys_alti_arrivee_coord">
+									<label for="sys_alt2">Système altimétrique</label>
+									<select name="sys_alt2" id="sys_alt" style="width:150px";>
+										<optgroup label="Suisse" class="Alt_suisse2">
+											<option value="RAN95" id="altimetrie_RAN95_coord2">RAN95</option>
+											<option value="NF02" id="altimetrie_NF02_coord2">NF02</option>
+										</optgroup>
+										<optgroup label="France" class="Alt_francais2">
+											<option value="IGN69" id="altimetrie_IGN69_coord2">IGN69</option>
+										</optgroup>
+									</select>
+								</div>
+							</div>
+							<div style="display:flex;">
+								<div>
+									<label for="Est2">Est [m]</label>
+									<input type="text" name="Est2" class="input_calc_coord" disabled>
+								</div>
+								<div>
+									<label for="Nord2">Nord [m]</label>
+									<input type="text" name="Nord2" class="input_calc_coord" disabled>
+								</div>
+								<div>
+									<label for="H2" id="label_input_alti_projetee_coord2">Hauteur [m]</label>
+									<input type="text" name="H2" class="input_calc_coord" disabled>
+								</div>
+								<div>
+									<label for="verticale" style="display:flex">Déviation de la véritcale &eta; - &xi;</label>
+									<input type="text" name="Vert1_2" id="vert" class="input_calc_coord" disabled>
+									<input type="text" name="Vert2_2" id="vert" class="input_calc_coord" disabled> 
+								</div>
+								<div>
+									<label for="Cote2">Cote du Géoid</label>
+									<input type="text" name="Cote2" class="input_calc_coord" disabled>
+								</div>
+							</div>
+						</div>
+						<div id="Geog2">
+							<div style="display:flex; margin:15px 0 0 0;">
+								<div>
+									<label for="Long2">Long</label>
+									<input type="text" name="Long2" class="input_calc_coord" disabled>
+								</div>
+								<div>
+									<label for="Lat2">Lat</label>
+									<input type="text" name="Lat2" class="input_calc_coord" disabled>
+								</div>
+								<div>
+									<label for="hauteur2">hauteur</label>
+									<input type="text" name="hauteur2" class="input_calc_coord" disabled>
+								</div>
+								<div>
+									<label for="geog_unite_arrivee">Unités</label>
+									<select name="geog_unite_arrivee" id="geog_unite_coord" style="width:150px";>
+										<option value="grade" id="geog_unite_coord_grade">Grades</option>
+										<option value="degre" id="geog_unite_coord_degre">Degrés centésimaux</option>
+										<option value="rad" id="geog_unite_coord_rad">Radians</option>
+									</select>
+								</div>
+							</div>
+						</div>
+						<div id="Cart2">
+							<div style="display:flex;margin:15px 0 0 0;">
+								<div>
+									<label for="X">X [m]</label>
+									<input type="text" name="X" class="input_calc_coord" disabled>
+								</div>
+								<div>
+									<label for="Y">Y [m]</label>
+									<input type="text" name="Y" class="input_calc_coord" disabled>
+								</div>
+								<div>
+									<label for="Z">Z [m]</label>
+									<input type="text" name="Z" class="input_calc_coord" disabled>
+								</div>
+							</div>
+						</div>
 					</fieldset>
+					<div style="display: flex;justify-content:space-between;">
+					<!--<input type="submit" class="button_popup_calc" value="CALCULER LES COORDONNÉES"/>
+					<input type="submit" class="button_popup_calc" value="AJOUTER À LA CARTE"/>-->
+					<div><p class="button_popup_calc">CALCULER LES COORDONNÉES<p/></div>
+					<div><p class="button_popup_calc">AJOUTER À LA CARTE<p/></div>
+					</div>
+					
 				</form>
 			</div>
 			<div id="trans_fichier" class="form_transfo">
@@ -153,22 +257,3 @@
 		</div>
 	</div>
 </div>
-<script type="text/javascript">
-var body_trans1 = document.getElementById('trans_coord');
-var body_trans2 = document.getElementById('trans_fichier');
-var head_trans1 = document.getElementById('head1');
-var head_trans2 = document.getElementById('head2');
-
-function show_trans_coord() {
-	body_trans1.style.display="block";
-	body_trans2.style.display="none";
-	head_trans1.style.borderWidth="0px 1px 0px 0px";
-	head_trans2.style.borderWidth="0px 0px 2px 1px";
-}
-function show_trans_fichier() {
-	body_trans2.style.display="block";
-	body_trans1.style.display="none";
-	head_trans1.style.borderWidth="0px 1px 2px 0px";
-	head_trans2.style.borderWidth="0px 0px 0px 1px";
-}
-</script>
