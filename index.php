123304aa8e24bcbfbc423dc41205367ef01debb0
brounst176 <bruno.dellacas@heig-vd.ch>
2018-05-03 10:24:20 +0200

    index

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 include("./php/vue/connexion_postgis.php");
 if (!isset($_SESSION)) session_start();
-<<<<<<< HEAD
+
 if(isset($_SESSION)){
 	if(isset($_SESSION['pseudo'])){
 			$res = pg_query($conn, "SELECT id_sess FROM session WHERE pseudo='".$_SESSION['pseudo']."';");
@@ -10,16 +10,6 @@ if(isset($_SESSION)){
 			$res = pg_query($conn, 'SELECT max(id_aleat) FROM "Points_session";');
 			$_SESSION['id_aleat'] = pg_fetch_result($res,0,0)+1;
 		}
-=======
-
-if(isset($_SESSION['pseudo'])){
-		$res = pg_query($conn, 'SELECT id_sess FROM session WHERE pseudo='.$_SESSION['pseudo'].';');
-		$_SESSION['id_sess'] = pg_fetch_result($res,0,0);
-}
-else{
-		$res = pg_query($conn, 'SELECT max(id_aleat) FROM "Points_session";');
-		$_SESSION['id_aleat'] = pg_fetch_result($res,0,0)+1;
->>>>>>> a2e9f91c93c80fec7c82a3c8d01ca8426462f475
 }
  ?>
 <?php
